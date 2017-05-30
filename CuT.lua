8db1460d7d4f80ad88e0ea53513f92c4bf58ddc7
Marco BARBIERI <marco.barbieri@unimore.it>
2017-05-30 12:04:56 +0200

    - CHANGED:      something begins to work...

--- CuT.lua
+++ CuT.lua
@@ -5,14 +5,11 @@
 --
 local addon, cut = ...
 
-cut.coinbase   =  {}
-
 local function getcoins()
    local coins =  {}
    local currency =  nil
    for currency, _ in pairs(Inspect.Currency.List()) do
       local detail = Inspect.Currency.Detail(currency)
---       coins[detail.name]=detail.stack
       coins[detail.name] = { stack=detail.stack, icon=detail.icon }
 --       print(string.format("CuT: (%s) (%s) (%s)", detail.name, detail.stack, detail.icon))
    end
@@ -20,35 +17,33 @@ local function getcoins()
    return coins
 end
 
-local function currencyevent()    
+local function initcoinbase()
+   print("InitCoinBase")
+   cut.coinbase   =  getcoins()
+   cut.baseinit   =  true
+
+   local a,b = nil, nil
+   for a,b in pairs(cut.coinbase) do print(string.format("CuT: cut.coinbase[%s]=%s", a, b)) end
+
+   return
+end
+
+local function currencyevent()
+
+   if cut.baseinit == false then initcoinbase() end
 
    local current  =  getcoins()
    local var, val =  nil, nil
    local tbl      =  {}
 
    -- find changes
---    for var, val in pairs(current) do
    for var, tbl in pairs(current) do
       val   =  tbl.stack
---       if val   ~= cut.coinbase[var] then
       if val   ~= (cut.coinbase[var].stack or 0) then
---          print(string.format("  CuT: check var=[%s]=>val[%s]", var, val))
---          local newvalue =  val - (cut.coinbase[var] or 0)
          local newvalue =  val - (cut.coinbase[var].stack or 0)
---          print(string.format("CuT: CHANGED var[%s](%s)(%s)=>(%s)", var, val, cut.coinbase[var], newvalue))
          cut.updatecurrencies(var, newvalue)
       end
    end
 end
 
-local function initcoinbase()
-   cut.coinbase   =  getcoins()
-   
-   -- now we can wait
-   Command.Event.Attach(Event.Currency,         function() currencyevent() end, "CuT Currency Event")   
-end
-
-
-
-Command.Event.Attach(Event.Addon.Load.End,   function() initcoinbase()  end, "CuT Initialize")
-
+Command.Event.Attach(Event.Currency,         function() currencyevent() end, "CuT Currency Event")
--- _cut_layout.lua
+++ _cut_layout.lua
@@ -5,27 +5,6 @@
 --
 local addon, cut = ...
 
-cut.addon               =  Inspect.Addon.Detail(Inspect.Addon.Current())["name"]
-cut.gui                 =  {}
-cut.gui.x               =  nil
-cut.gui.y               =  nil
-cut.gui.width           =  350
-cut.gui.height          =  100
-cut.gui.borders         =  {}
-cut.gui.borders.left    =  4
-cut.gui.borders.right   =  4
-cut.gui.borders.bottom  =  4
-cut.gui.borders.top     =  4
-cut.gui.font            =  {}
-cut.gui.font.size       =  14
-cut.gui.font.name       =  "fonts/MonospaceTypewriter.ttf"
---
-cut.shown               =  {}
-cut.shown.objs          =  {}
-cut.shown.objs.count    =  0
-cut.shown.objs.last     =  nil
-cut.frames              =  {}
-
 local function updateguicoordinates(win, x, y)
 
    if win ~= nil then
@@ -94,30 +73,33 @@ local function createnewline(currency, value)
    currencylabel:SetText(string.format("%s:", currency))
    currencylabel:SetLayer(3)
    currencylabel:SetPoint("TOPLEFT",   currencyframe, "TOPLEFT", cut.gui.borders.left, 0)
- 
-   local currencyicon = UI.CreateFrame("Texture", "currency_icon_" .. currency, currencyframe)
-   currencyicon:SetTexture("Rift", (cut.coinbase[currency].currencyicon or "reward_gold.png.dds"))
-   currencyicon:SetWidth(cut.gui.font.size)
-   currencyicon:SetHeight(cut.gui.font.size)
-   currencyicon:SetLayer(3)
-   currencyicon:SetPoint("TOPRIGHT",   currencyframe, "TOPRIGHT", -cut.gui.borders.right, 0)    
 
    if currency == "Platinum, Gold, Silver" then value = cut.printmoney(value) end
-   
+
    local currencyvalue  =  UI.CreateFrame("Text", "currency_value_" .. currency, currencyframe)
    currencyvalue:SetFont(cut.addon, cut.gui.font.name)
    currencyvalue:SetFontSize(cut.gui.font.size )
    currencyvalue:SetText(string.format("%s", value), true)
    currencyvalue:SetLayer(3)
-   currencyvalue:SetPoint("TOPRIGHT",   currencyicon, "TOPLEFT", -cut.gui.borders.right, -2)
-   
+   currencyvalue:SetPoint("TOPLEFT",   currencylabel, "TOPRIGHT", cut.gui.borders.left, 0)
+
+   local currencyicon = UI.CreateFrame("Texture", "currency_icon_" .. currency, currencyframe)
+   currencyicon:SetTexture("Rift", (cut.coinbase[currency].icon or "reward_gold.png.dds"))
+   currencyicon:SetWidth(cut.gui.font.size  + 2)
+   currencyicon:SetHeight(cut.gui.font.size + 2)
+   currencyicon:SetLayer(3)
+   currencyicon:SetPoint("TOPLEFT",   currencyvalue, "TOPRIGHT", -cut.gui.borders.right, 0)
+
+
    cut.shown.objs[currency]   =  currencyvalue
-   cut.shown.objs.count  = 1 + cut.shown.objs.count   -- last shown by number
-   cut.shown.objs.last =  currency                    -- last shown by currecny name
-   
+
+--    cut.shown.frames[currency] =  currencyframe
+   cut.shown.frames.count     =  1 + cut.shown.frames.count -- last frame shown by number
+--    cut.shown.frames.last      =  currencyframe              -- last frame shown by currecny name
+
 --    local a,b = nil, nil
 --    for a,b in pairs(cut.shown.objs) do print(string.format("CuT: cut.shown.objs.%s=%s", a, b)) end
-   
+
    return currencyframe
 
 end
@@ -136,24 +118,53 @@ function cut.updatecurrencies(currency, value)
 
    if not cut.gui.window then cut.gui.window = createwindow() end
 
-   if cut.shown.objs.currency then
+   if cut.shown.objs[currency] then
       print("...UPDATING...")
       updatecurrencyvalue(currency, value)
    else
       print("...CREATING...")
       local newline =   createnewline(currency, value)
-      if cut.shown.objs.count > 1  then
---          print("NOT First currencies")
-         newline:SetPoint("TOPLEFT",   cut.shown.objs[cut.shown.objs.last], "BOTTOMLEFT")
-         newline:SetPoint("TOPRIGHT",  cut.shown.objs[cut.shown.objs.last], "BOTTOMRIGHT")
+      if cut.shown.frames.count > 1  then
+         print("NOT First currencies")
+         newline:SetPoint("TOPLEFT",   cut.shown.frames.last, "BOTTOMLEFT")
+         newline:SetPoint("TOPRIGHT",  cut.shown.frames.last, "BOTTOMRIGHT")
       else
---          print("First currencies")
+         print("First currencies")
          newline:SetPoint("TOPLEFT",   cut.frames.container,   "TOPLEFT",  cut.gui.borders.left,   cut.gui.borders.top)
          newline:SetPoint("TOPRIGHT",  cut.frames.container,   "TOPRIGHT", -cut.gui.borders.right, cut.gui.borders.top)
       end
---       cut.shown.objs.currency = newline
-      cut.shown.objs[currency] = newline
+
+      cut.shown.frames.last   =  newline
+
+--       cut.shown.objs[currency] = newline
    end
 
    return
 end
+
+
+
+--[[
+Error: Incorrect function usage.
+  Parameters: (userdata: 59f5f450), "TOPLEFT", nil, "BOTTOMLEFT"
+  Parameter types: userdata, string, nil, string
+Function documentation:
+	Pins a point on this frame to a location on another frame. This is a rather complex function and you should look at examples to see how to use it.
+	This function can take many different forms. In general, it looks like this: SetPoint(point_on_this_frame, target_frame, point_on_target_frame [, x_offset, y_offset]).
+	The first part is the point on this frame that will be attached. Usually, these are string identifiers. "TOPLEFT", "TOPCENTER", "TOPRIGHT", "CENTERLEFT", "CENTER", "CENTERRIGHT", "BOTTOMLEFT", "BOTTOMCENTER", "BOTTOMRIGHT". You may also use a string identifier that refers to a single axis - "TOP", "BOTTOM", "LEFT", "RIGHT", "CENTERX", "CENTERY". If you want more direct numeric control you can use number pairs. 0,0 is equivalent to "TOPLEFT", 1,1 is equivalent to "BOTTOMRIGHT", 0.5,nil is equivalent to "CENTERX".
+	The second part is the frame to attach to, as well as the point on that frame to attach to. The frame is simply passing in the frame table. The point is the same identifier or number pair as the first parameter.
+	Optionally, you may include an X or Y offset to the point.
+	This connection is permanent, and if the target frame moves, this frame will move along with it.
+	Caveat: If the target is a frame set to the "restricted" SecureMode, and the client is currently in "secure" mode, then unexpected behavior may occur.
+	Not permitted on a frame with "restricted" SecureMode while the addon environment is secured.
+		Frame:SetPoint(...)   -- ...
+Parameters:
+		...:	This function's parameters are complicated. Read the above summary for details.
+    In CuT / CuT Currency Event, event Event.Currency
+stack traceback:
+	[C]: ?
+	[C]: in function 'SetPoint'
+	CuT/_cut_layout.lua:129: in function 'updatecurrencies'
+	CuT/CuT.lua:44: in function 'currencyevent'
+	CuT/CuT.lua:52: in function <CuT/CuT.lua:52>
+         ]]--
